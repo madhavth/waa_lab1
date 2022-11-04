07720a245f7ae59373c0726425f94ea47115deff
madhavth <madhavth77@gmail.com>
2022-11-03 19:41:28 -0500

    moved lab1 to lab folder

---
+++ lab1/src/main/java/com/example/lab1/domain/NotFound.java
@@ -0,0 +1,12 @@
+package com.example.lab1.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class NotFound {
+    public String message;
+}
