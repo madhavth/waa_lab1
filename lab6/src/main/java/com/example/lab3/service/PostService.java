f24619168078023e099f052942398ece35e0d2b4
madhavth <madhavth77@gmail.com>
2022-11-13 15:31:25 -0600

    added lab6

--- lab6/src/main/java/com/example/lab3/service/PostService.java
+++
@@ -1,9 +0,0 @@
-package com.example.lab3.service;
-
-import com.example.lab3.domain.dto.response.PostDto;
-
-import java.util.List;
-
-public interface PostService {
-   public List<PostDto> getPostsWithTitle(String title);
-}
--- lab6/src/main/resources/data.sql
+++ lab6/src/main/resources/data.sql
@@ -1,10 +1,1 @@
-INSERT INTO roles (id, role)
-values (1, 'USER');
-INSERT INTO roles (id, role)
-values (2, 'ADMIN');
-
-
-INSERT INTO users (id, name, password)
-values (1,
-        'madhav',
-        '$2a$10$Lt5brLr2vSe.jmZ32Cs8A.eS5ureo8BjyagvA5rj7yLoXRALgZnH6');
+INSERT INTO role (name) VALUES ('ADMIN'), ('USER');
