867c279fc3c625d08d331a7de39860a5e53fe3ea
madhavth <madhavth77@gmail.com>
2022-11-15 14:44:02 -0600

    data.sql added

---
+++ lab3/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into users (id, name) values( 100, 'Madhav');
+
+insert into posts (content, title, user_id) values('content 1', 'title 1', 100);
+insert into posts (content, title, user_id) values('content 2', 'title 2', 100);
+insert into posts (content, title, user_id) values('content 3', 'title 3', 100);
